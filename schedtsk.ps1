c8e433184b3c73cfcfe7fc2a9794b51e5b5720cc
JH517 <jordanhyde@live.co.uk>
2025-04-02 15:12:47 +0100

    Update schedtsk.ps1

--- schedtsk.ps1
+++ schedtsk.ps1
@@ -1,8 +1,8 @@
+# Define the trigger (every 4 hours)
+$Trigger = New-ScheduledTaskTrigger -Once -At (Get-Date).AddMinutes(1) -RepetitionInterval (New-TimeSpan -Hours 4) -RepetitionDuration ([TimeSpan]::MaxValue)
+
 # Define the action (PowerShell command to run)
 $Action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-ExecutionPolicy Bypass -Command `"Start-Process chrome.exe '--new-window https://sharepointmaven.com/wp-content/uploads/2022/09/brandloginwindowmicrosoft3651.jpg' -WindowStyle Normal`""
-
-# Define the trigger (every 4 hours)
-$Trigger = New-ScheduledTaskTrigger -Daily -RepetitionInterval (New-TimeSpan -Hours 4) -RepetitionDuration (New-TimeSpan -Days 1) -At "00:00"
 
 # Define the task settings (task configuration)
 $Settings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries -StartWhenAvailable
